00268a8d1f72da4c1cd1f05ec19ae6e6667ef002
jason-aultman <jason.aultman@gmail.com>
2020-08-10 20:47:13 -0400

    added class and refactored to FileUtility.cs class

---
+++ FileUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Aug8thExamples
+{
+    public static class FileUtility
+    {
+        public static void CreateFile(string name)
+        {
+            if (!File.Exists(name))
+            {
+                File.Create(name);
+            }
+        }
+
+        public static void WriteToFile(string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                writer.WriteLine("This is a new string");
+            }
+        }
+
+        public static void ReadFromfile(string filename)
+        {
+            using (StreamReader streamReader = new StreamReader(filename))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Aug8thExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "jasonsFile";
+            FileUtility.CreateFile(name);
+            FileUtility.WriteToFile(name);
+            FileUtility.ReadFromfile(name);
+        }
+
+       
+    }
+}
